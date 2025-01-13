79c6fc3e86b9c290b5924d5da2e2018213f96314
Walter Prince <wprince0288@gmail.com>
2025-01-12 20:59:51 -0600

    middleware1

--- api/server.js
+++ api/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
-
+const { logger } = require('./middleware/middleware');
 const server = express();
 
 // remember express by default cannot parse JSON in request bodies
+server.use(express.json());
 
+server.use(logger)
 // global middlewares and the user's router need to be connected here
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // require your server and launch it
+const server = require('./api/server');
+
+const PORT = 9000;
+
+server.listen(PORT, () => {
+    console.log(`\n***server Running on http://localhost:${PORT} ***\n`);
+});
