ca1d225edd365b002cc7beee2a3394132b3b2fda
Walter Prince <wprince0288@gmail.com>
2025-01-14 18:49:54 -0600

    000111101010110

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -8,22 +8,34 @@ const {
 // You will need `users-model.js` and `posts-model.js` both
 // The middleware functions also need to be required
 
+const User = require('./users-model')
+const Post = require('../posts/posts-model')
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
   // RETURN AN ARRAY WITH ALL THE USERS
+  User.get()
+    .then(users => {
+      res.json(users)
+    })
+    .catch(next)
 });
 
 router.get('/:id', validateUserId, (req, res) => {
   // RETURN THE USER OBJECT
   // this needs a middleware to verify user id
-  console.log(req.user)
+  res.json(req.user)
 });
 
-router.post('/', validateUser, (req, res) => {
+router.post('/', validateUser, (req, res, next) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
-  console.log(req.name)
+  User.insert({ name: req.name })
+    .then(newUser => {
+      res.status(201).json(newUser)
+    })
+    .catch(next)
 });
 
 router.put('/:id', validateUserId, validateUser, (req, res) => {
@@ -53,6 +65,14 @@ router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
   console.log(req.user)
   console.log(req.text)
 });
+
+router.use((err, req, res, next) => { //eslint-disable-line
+  res.status(err.status || 500).json({
+    customMessage: 'something tragic inside posts router happened',
+    message: err.message,
+    stack: err.stack,
+  })
+})
 
 // do not forget to export the router
 module.exports = router
